0a903ea9aed372124c7c248ec752aa747e3cf163
Helge Wieding <helge@wieding.com>
2018-11-16 17:27:41 +0100

    added cb for Generate Event

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,8 +1,15 @@
 import { Component, OnInit, InjectionToken, Inject } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-declare let Web3: any;
 import { environment } from '../environments/environment';
 import { abis } from '../abis';
+import { switchMap } from 'rxjs/operators';
+import { of } from 'rxjs/Observable/of';
+
+declare let Web3: any;
+
+declare global {
+  interface Window { web3: any; }
+}
 
 
 @Component({
@@ -16,6 +23,10 @@ export class AppComponent implements OnInit {
   merkleMine: any;
 
   protected address: string;
+  protected error: string;
+  protected success: string;
+  protected proof: any;
+  protected loading: boolean;
 
   constructor(private http: HttpClient) {
 
@@ -24,25 +35,41 @@ export class AppComponent implements OnInit {
     this.web3 = new Web3(window.web3.currentProvider);
     const contract = this.web3.eth.contract(abis[0]);
     this.merkleMine = contract.at(environment.contract_addresses[0]);
-    console.log(this.web3);
+    console.log(this.merkleMine);
   }
 
   getProof(add: string) {
     if (!add) {
-      // show error
+      this.error = 'Please enter a valid address';
       return;
     }
-    this.http.get(`${environment.base_url}${add}`).subscribe((v: any) => {
+    this.loading = true;
+    this.http.get(`${environment.base_url}${add}`)
+    .subscribe((v: any) => {
+      console.log(v);
       if (v !== null) {
-        console.log(v.address.toLowerCase());
-        this.merkleMine.generate(v.address.toLowerCase(), v.proof, (e) => {
-          console.log(e);
+        this.proof = v.proof;
+        this.merkleMine.generated(add.toLocaleLowerCase(), (error, claimed) => {
+          this.loading = false;
+          if (!claimed) {
+            this.error = 'Amount already claimed';
+          } else {
+            this.success = 'Congratulations! Your address has been found! Claim your tokens now!';
+          }
         });
       } else {
-        // error case
+        this.loading = false;
+        this.error = 'Sorry, Your address does not exist!';
       }
-
     });
   }
 
+  claim(addr) {
+    this.merkleMine.generate(addr.toLowerCase(), this.proof, (e, val) => {
+      const address = val;
+      this.merkleMine.Generate().watch({filter: { address }}, () => {
+        // event mined. user has been paid out;
+      });
+    });
+  }
 }
